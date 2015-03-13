180acb410417e945b2b45709ca5fad777463c8df
Emily Atkinson <eatkinson51894@yahoo.com>
2015-03-13 16:20:10 -0400

    Binary Search WordsArray updated

--- src/BinarySearchWordsArray.java
+++ src/BinarySearchWordsArray.java
@@ -4,49 +4,43 @@ import java.util.*;
 
 public class BinarySearchWordsArray {
 
-	HTMLWordsArray hwa   = new HTMLWordsArray();
-	   
-	    	 
-	public static void main(String[] args) {
-		  HTMLWordsArray hwa  = new HTMLWordsArray();
-		  String[] html = hwa.htmlArray;
-		  
-		  
-		  Readers r = new Readers(); 
-		  String[] wordsArray = r.wordsArray; 
-		  
-	   for (int i = 0; i < html.length; i++) {
+	HTMLWordsArray hwa = new HTMLWordsArray();
+	String[] html = hwa.htmlArray;
+	LinkedList<String> matchedWords;   
+
+	for (int i = 0; i < html.length; i++) {
 		   	
 		   	String key = html[i];  
-	        int lastElementPosition = (html.length -1);
-	        int firstElementPosition = 0; 
-	        
-	      
-	        Arrays.sort(wordsArray);
-	        binarySearch (wordsArray, key); {
-	      
-	  }
-	   
-	 
-public static void binarySearch(String[] array, String key) {
+	        int max= (html.length -1);
+	        int min = 0; 
 	       
-	int min = 0; 
-	int max = array.length;
-	LinkedList<String> matchedWords;  
+	        binarySearch(wordsArray, key);
+		}
+		  
+		  
+private void binarySearch(String[] array, String key) {
+	       
+		int min = 0; 
+		int max = array.length;
+		
+	                                      
+	    while (min <= max) {
+	         int i = (min + max) / 2;
+	         if (array[i].compareTo(key) < 0) {
+	                min = i + 1;
+	            } else if (array[i].compareTo(key) > 0) {
+	                max = i - 1;
+	            } else {
+	                matchedWords.add(key); 
+	                
+	            }
+	        }    
+		    		 
+	}
+
+
+		 
+	 
 	
-                                      
-    while (min <= max) {
-         int i = (min + max) / 2;
-         if (array[i].compareTo(key) < 0) {
-                min = i + 1;
-            } else if (array[i].compareTo(key) > 0) {
-                max = i - 1;
-            } else {
-                matchedWords.add(key); 
-                
-            }
-        }    
-	     
-
-
+	}
 }
